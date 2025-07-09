6be771b4bdb54fc067e2f075e7dabf367bcb22f5
Ulyana Shandruk <ulyana.shandruk@gmail.com>
2025-07-09 15:03:30 +0300

    Обновлён .gitignore

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,6 @@ public class Main {
     }*/
     public static void main(String[] args)
     {
-        System.out.println("Привет, Мир!");
+        System.out.println("Привет, Мир! Сделано ДЗ1!");
     }
 }
